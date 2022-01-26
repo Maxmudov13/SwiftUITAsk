4cf6426d41a546efedaaf2c777fc938d224a46ef
iMtRo <maxmudovilyos75@gmail.com>
2022-01-26 15:34:52 +0500

    Initial Commit

---
+++ SwiftUITAsk/SwiftUITAskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITAskApp.swift
+//  SwiftUITAsk
+//
+//  Created by user on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITAskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
